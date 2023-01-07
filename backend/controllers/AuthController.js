c947b0c514e47fd20a1ed67308872d1df94c4aa5
Mohamed-Laghbar <simoghbaar@gmail.com>
2023-01-07 22:52:43 +0100

    CreateError function to store the message and the statusCode before send it to the middleware

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,17 +1,21 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import  CreateError from '../utils/Error.js'
 
 const login = async (req, res, next) => {
-
     try {
-        const {email} = res.bo
-    res.json('login function')
-    
-} catch (error) {
-next(error)    
-}
+        const { email, password } = req.body;
+        // check if the user exist
+        const user = User.findOne({ email });
+        if(!user) return next(CreateError('Invalid Email', 400))
 
-}
+       const isMatch = bcrypt.compare(password,user.password);
 
+       if(!isMatch) return next(CreateError('Wrong Password', 400));
 
+       
+        
+    } catch (error) { }
+};
 
-export default login
+export default login;
