57f8fa15b17a2716e1d6d0cc3ca8b9f33d116414
Mohamed-Laghbar <simoghbaar@gmail.com>
2023-01-08 01:15:52 +0100

    detect the role of the currunt user and redirect  him depends on his role with the acces token and the refresh token

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,47 +1,69 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
-import CreateError from '../utils/Error.js'
-import GenerateToken from '../utils/token.js'
+import CreateError from "../utils/Error.js";
+import generateToken from "../utils/token.js";
 
 const login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         // check if the user exist
         const user = await User.findOne({ email });
-        if (!user) return next(CreateError('Invalid Email', 400))
+        if (!user) return next(CreateError("Invalid Email", 400));
         req.user = user;
-
 
         // const isMatch = bcrypt.compare(password, user.password);
         const isMatch = password === user.password;
 
-        if (!isMatch) return next(CreateError('Wrong Password', 400));
+        if (!isMatch) return next(CreateError("Wrong Password", 400));
 
-        //    get the role of that user to generate token accordenly 
+        //    get the role of that user to generate token accordenly
         const { role } = req.user;
+        let token = generateToken(req.user);
 
         switch (role) {
-            case 'user':
+            case "user":
+                await User.findByIdAndUpdate(user._id, {
+                    refresh_Token: token.user_refresh_token,
+                });
+                req.user.acces_token = token.user_acces_token;
+                await res.cookie("refresh_token", token.user_refresh_token, {
 
-                const { user_acces_token, user_refresh_token } = GenerateToken(req.user);
-                res.json(`user token is ${user_refresh_token, user_acces_token}`)
+                    httpOnly: true,
+                    maxAge: 7 * 24 * 60 * 60,
+                    sameSite: "none",
+                    secure: false,
+                });
+                res.status(200).json({
+                    success: true,
+                    token: req.user.acces_token,
+                    message: "Login successful",
+                });
 
                 break;
 
-            case 'admin':
-                const { admin_acces_token, admin_refresh_token } = GenerateToken(user);
-                res.json(`admin token is ${admin_acces_token, admin_refresh_token}`);
-                
+            case "admin":
+                await User.findByIdAndUpdate(user._id, {
+                    refresh_Token: token.admin_refresh_token,
+                });
+                req.user.acces_token = token.admin_acces_token;
+                res.cookie("refresh_token", token.admin_refresh_token, {
+                    httpOnly: true,
+                    maxAge: 7 * 24 * 60 * 60,
+                    sameSite: "none",
+                    secure: false,
+                });
+                res.status(200).json({
+                    success: true,
+                    token: req.user.acces_token,
+                    message: "Login successful",
+                });
+
                 break;
 
-            default:
-                break;
+
         }
-
-
-
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
 
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,34 +1,42 @@
 import jwt from "jsonwebtoken";
-const GenerateToken = (user) => {
+const generateToken = (user) => {
     // create acces token for users
     const user_acces_token = jwt.sign(
         { id: user._id, email: user.email },
         process.env.USER_JWT_SECRET || "",
-        { expiresIn: "1d" }
+        { expiresIn: "1h" }
     );
 
     // create refresh token for users
     const user_refresh_token = jwt.sign(
         { id: user._id, email: user.email },
         process.env.USER_REFRESH_SECRET,
-        { expiresIn: "5d" }
+        { expiresIn: "1d" }
     );
 
     // create acces token for admin
     const admin_acces_token = jwt.sign(
         { id: user._id, email: user.email },
         process.env.ADMIN_JWT_SECRET || "",
-        { expiresIn: "1d" }
+        { expiresIn: "1h" }
     );
 
     // create refresh token for admin
     const admin_refresh_token = jwt.sign(
         { id: user._id, email: user.email },
         process.env.ADMIN_REFRESH_SECRET || "",
-        { expiresIn: "5d" }
+        { expiresIn: "1d" }
     );
     // console.log( user_acces_token, user_refresh_token, admin_acces_token, admin_refresh_token);
 
-    return  user_acces_token, user_refresh_token, admin_acces_token, admin_refresh_token;
+    const Tokens = {
+        user_acces_token,
+        user_refresh_token,
+        admin_acces_token, 
+        admin_refresh_token
+    }
+    // console.log(Tokens.admin_acces_token);
+
+    return Tokens;
 };
-export default GenerateToken;
+export default generateToken;
