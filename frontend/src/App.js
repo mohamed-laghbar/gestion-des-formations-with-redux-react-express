74c3c15fc321c423100a2804a4b7ec6d5821fb5a
Mohamed-Laghbar <simoghbaar@gmail.com>
2023-01-12 23:51:15 +0100

    create login component and store the user globaly using useContext api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import Login from './pages/Login';
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-    <Login/>
+      <Login />
     </div>
   );
 }
